087dd6ab053e076aa94a5927781cc74bc57c8a65
Georgina Sarpong <georginasarpong.student@careerdevs.com>
2019-04-29 23:10:43 +0000

    part1

---
+++ script.js
@@ -0,0 +1,5 @@
+var myName = "Georgina";
+var fruitName = "mango";
+var fruitPrice = 5;
+
+console.log("Hello", myName, "the cost of", fruitName, "cost","$" + fruitPrice);
---
+++ script01.js
@@ -0,0 +1,7 @@
+var myName = "Georgina";
+var fruitName = "mango";
+var fruitPrice = 5
+var totalFruitOrdered = 11;
+var totalCostOfFruit = totalFruitOrdered * fruitPrice;
+
+console.log("Hello", myName, "the cost of", totalFruitOrdered, fruitName, "cost","$" + totalCostOfFruit);
