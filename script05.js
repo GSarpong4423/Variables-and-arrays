d39c86f5cc310d9c55f6950ef32f7f80dd86be1a
Georgina Sarpong <georginasarpong.student@careerdevs.com>
2019-05-02 23:09:23 +0000

    array 2 for array homework complete

--- script05.js
+++ script05.js
@@ -7,4 +7,12 @@ fruits1.push("peppers");
 fruits1.pop();
 
 
-// 
+// array 1 complete
+
+var fruits2 = ["kiwis", "oranges"];
+fruits2.push("Sumo Orange");
+fruits2.push("Grapes");
+fruits2.shift();
+fruits2[2] = "Egg Plant";
+
+// array 2 complete
