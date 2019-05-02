4afcf7e971cb6f6e7e48c46a73b0e5563bec804b
Georgina Sarpong <georginasarpong.student@careerdevs.com>
2019-05-02 23:01:26 +0000

    1st part of the array homework

---
+++ script05.js
@@ -0,0 +1,10 @@
+var fruits1 = ["Apple", "Peaches"];
+console.log(fruits1);
+fruits1[1] = "Strawberries";
+fruits1.push("pumpkin")
+console.log(fruits1);
+fruits1.push("peppers");
+fruits1.pop();
+
+
+// 
