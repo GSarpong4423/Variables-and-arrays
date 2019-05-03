fd8b8373e8fb243c156c71e6f200cf3891b111ef
Georgina Sarpong <georginasarpong.student@careerdevs.com>
2019-05-03 01:25:12 +0000

    for loop completed

--- script05.js
+++ script05.js
@@ -16,3 +16,14 @@ fruits2.shift();
 fruits2[2] = "Egg Plant";
 
 // array 2 complete
+
+var allFruits = fruits1.concat(fruits2);
+var fruitPrices = [1, 1.5, 2, 2.5, 3, 3.5];
+fruitPrices.push(4);
+
+var fruitSold = [4, 10, 2, 6, 4, 16];
+
+for (var i = 0; i < allFruits.length; i++) {
+    var fruitXTotal =  fruitPrices[i] * fruitSold[i];
+     console.log("the total sales for", fruitSold[i],allFruits[i], "$" + fruitXTotal);
+}
